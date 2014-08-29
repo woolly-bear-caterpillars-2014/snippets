49e1f16d521610ba560b265e2800bb13f8443ff6
Edwardo Kimmy & garrett dalton <pair+eddiekimosabe.garrettdalton@devbootcamp.com>
2014-08-29 15:14:32 -0400

    Remove --warning from .rspec file. Uncommented the user_spec code and added an end so that rspec could run.

--- snippets/spec/models/user_spec.rb
+++ snippets/spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-# describe User do
-#   let!(:user) { User.new(email: "test@sbctesting.com", password: "testing123") }
+describe User do
+  let!(:user) { User.new(email: "test@sbctesting.com", password: "testing123") }
 
-#   it "should have many and respond to cheatsheets" do
-#     expect(:user).to respond_to()
+  it "should have many and respond to cheatsheets" do
+    expect(:user).to respond_to()
+  end
 end
