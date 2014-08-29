98ee2c0b0c5f1a8ee856f1241961da6ba4f0c568
Alexander Le & David Bragdon <pair+alextle0125.postmodem@devbootcamp.com>
2014-08-29 11:07:55 -0400

    Frame out action methods in CheatsheetsController

--- snippets/app/controllers/cheatsheets_controller.rb
+++ snippets/app/controllers/cheatsheets_controller.rb
@@ -1,2 +1,26 @@
 class CheatsheetsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
