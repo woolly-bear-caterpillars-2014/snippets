c6d865584a1424e13d21e40bc192676fae39b5f0
Edwardo Kimmy & garrett dalton <pair+eddiekimosabe.garrettdalton@devbootcamp.com>
2014-08-29 15:35:57 -0400

    setup capybara, no temper tantrums from machine

---
+++ snippets/spec/features/user_creates_cheatsheet_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "the signin process", :type => :feature do
+  before :each do
+    User.make(:email => 'user@example.com', :password => 'caplin')
+  end
+
+  it "signs me in" do
+    visit '/sessions/new'
+    within("#session") do
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Success'
+  end
+end
--- snippets/spec/spec_helper.rb
+++ snippets/spec/spec_helper.rb
@@ -5,6 +5,7 @@
 #
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
+require 'capybara/rspec'
 # will add to the boot time of your test suite on EVERY test run, even for an
 # individual file that may not need all of that loaded. Instead, make a
 # separate helper file that requires this one and then use it only in the specs
@@ -15,6 +16,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
