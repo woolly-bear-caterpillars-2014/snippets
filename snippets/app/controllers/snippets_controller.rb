6a754ccc243a8f00ed90b5224c0a83ee9f1a4cec
Audrey McGowan <audrey.b.mcgowan@gmail.com>
2014-08-29 10:11:34 -0400

    added basic controller for snippets

---
+++ snippets/app/controllers/snippets_controller.rb
@@ -0,0 +1,22 @@
+class SnippetsController < ApplicationController
+	def index
+		@snippets = Snippet.all
+	end
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
