9fc369d979c65d99c29353d239913af4d18aa241
Alexander Le <alextle0125@gmail.com>
2014-08-30 21:33:37 -0400

    only author of snippet can edit or delete snippet

--- snippets/app/models/snippet.rb
+++ snippets/app/models/snippet.rb
@@ -2,7 +2,7 @@ class Snippet < ActiveRecord::Base
 
   validates :language, presence: true
 
-	belongs_to :user
+	belongs_to :author, class_name: "User", foreign_key: :user_id
 	has_many :cheatsheet_snippets
 	has_many :cheatsheets, through: :cheatsheet_snippets
 	has_many :snippet_tags
